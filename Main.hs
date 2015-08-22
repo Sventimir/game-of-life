f9d9b0de7608435a55f9b261eb171170f7e87619
Sventimir <svantimir@gmail.com>
2015-08-22 23:40:28 +0200

    Show many generations in a loop.

--- Main.hs
+++ Main.hs
@@ -1,21 +1,29 @@
 module Main where
 
-import Control.Monad.State (runState)
+import Control.Monad.State (StateT, runStateT, liftIO, get)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-import Interface.Board (next)
+import Interface.Board (Cell, next)
 import Data.Board (Board(..))
 
 
-main :: IO ()
+main :: IO (Board, Board)
 main = do
     putStrLn "Hello Life!"
-    print $ current initial
-    print . current . snd $ runState next initial
-    print . current . snd $ runState (next >> next) initial
+    runStateT (loop 10 showNext) initial
 
 initial :: Board
 initial = Board (Set.fromList init) Nothing
     where
     init = zip [(-1)..1] $ repeat 0
+
+
+loop :: Int -> StateT Board IO Board -> StateT Board IO Board
+loop i s
+    | i > 0 = s >> loop (pred i) s
+    | otherwise = s >> get >>= return
+
+showNext :: StateT Board IO Board
+showNext = next >>= \b -> liftIO (print b >> return b)
+
