8512e805479a4350457fb90d81bf682ebbe5e5ca
Sventimir <svantimir@gmail.com>
2015-08-22 19:24:19 +0200

    Ad hoc manual tests.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Control.Monad.State (runState)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Interface.Board (next)
+import Data.Board (Board(..))
+
+
 main :: IO ()
-main = putStrLn "Hello life!"
+main = do
+    putStrLn "Hello Life!"
+    print $ current initial
+    print . current . snd $ runState next initial
+    print . current . snd $ runState (next >> next) initial
+
+initial :: Board
+initial = Board (Set.fromList init) Nothing
+    where
+    init = zip [(-1)..1] $ repeat 0
