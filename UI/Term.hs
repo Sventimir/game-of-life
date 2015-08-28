36049088a91af6ba605cdd784ba83496062b8d3c
Sventimir <svantimir@gmail.com>
2015-08-28 22:45:31 +0200

    Restore reverted changes introducing State Monad into the system.

--- UI/Term.hs
+++ UI/Term.hs
@@ -6,6 +6,7 @@ module UI.Term (
 ) where
 
 import Control.Monad (liftM)
+import Control.Monad.State (StateT, runStateT, get, lift)
 import qualified UI.NCurses as Curses
 
 import Interface.Board (BoardState(..), Cell)
@@ -14,6 +15,7 @@ import Interface.Board (BoardState(..), Cell)
 data DisplayState = DisplayState {
         win             :: Curses.Window
     }
+type AlterDisplay a = StateT DisplayState Curses.Curses a
 
 display :: Curses.Curses a -> IO a
 display = Curses.runCurses
@@ -23,8 +25,11 @@ initializeDisplay :: BoardState b => b -> Curses.Curses DisplayState
 initializeDisplay b = Curses.setEcho False >> liftM DisplayState Curses.defaultWindow
 
 
-render :: DisplayState -> Curses.Curses DisplayState
-render dispState = Curses.updateWindow (win dispState) update >> Curses.render >> return dispState
+render :: AlterDisplay ()
+render = do
+        dispState <- get
+        lift $ Curses.updateWindow (win dispState) update
+        lift $ Curses.render
     where
     update = do
         Curses.moveCursor 1 10
@@ -34,14 +39,14 @@ render dispState = Curses.updateWindow (win dispState) update >> Curses.render >
         Curses.moveCursor 0 0
 
 
-mainLoop :: DisplayState -> Curses.Curses DisplayState
-mainLoop dispState = waitFor (win dispState) >> return dispState
+mainLoop :: AlterDisplay ()
+mainLoop = get >>= (\dispState -> lift $ waitFor (win dispState))
 
 waitFor :: Curses.Window -> Curses.Curses ()
 waitFor w = Curses.getEvent w Nothing >>= handleEvent
     where
-    oneOf e chrs = e `elem` map Curses.EventCharacter chrs
+    oneOfChars e chrs = e `elem` map Curses.EventCharacter chrs
     handleEvent Nothing = waitFor w
     handleEvent (Just ev)
-        | ev `oneOf` ['q', 'Q'] = return ()
+        | ev `oneOfChars` ['q', 'Q'] = return ()
         | otherwise = waitFor w
