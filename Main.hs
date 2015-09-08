ab39353a0771a47bc63d1f673c58186cc386ff19
Sventimir <svantimir@gmail.com>
2015-09-08 21:22:54 +0200

    Start development of GTK GUI.

--- Main.hs
+++ Main.hs
@@ -1,12 +1,11 @@
 module Main where
 
-import Control.Monad.State (runStateT, lift)
-import Data.Set (Set)
-import qualified Data.Set as Set
 
-import Interface.Board (Cell, next)
-import Data.Board (Board(..))
+import Graphics.UI.Gtk
 
+main = do
+   initGUI
+   window <- windowNew
+   widgetShowAll window
+   mainGUI
 
-main :: IO ()
-main = putStrLn "Hello World!"
