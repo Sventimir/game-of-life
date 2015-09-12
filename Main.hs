67f945e2f50563ecd1983f32cc7e80b818933f2f
Sventimir <svantimir@gmail.com>
2015-09-12 22:21:07 +0200

    Add GUI created with Glade.

--- Main.hs
+++ Main.hs
@@ -1,11 +1,21 @@
 module Main where
 
+import Control.Monad.Trans (liftIO)
 
-import Graphics.UI.Gtk
+import qualified Graphics.UI.Gtk as GTK
+import Graphics.UI.Gtk.Builder (builderNew, builderAddFromFile, builderGetObject)
+import qualified Graphics.Rendering.Cairo as Cairo
 
+
+main :: IO ()
 main = do
-   initGUI
-   window <- windowNew
-   widgetShowAll window
-   mainGUI
+        GTK.initGUI
 
+        builder <- builderNew
+        builderAddFromFile builder "UI/life.glade"
+
+        mainWin <- builderGetObject builder GTK.castToWindow "mainWin"
+        mainWin `GTK.on` GTK.deleteEvent $ liftIO GTK.mainQuit >> return False
+
+        GTK.widgetShowAll mainWin
+        GTK.mainGUI
